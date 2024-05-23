0dce9e780e03c4f1aba2b16bca9d777be1953a93
SeHwan Lim <76430979+Torres-09@users.noreply.github.com>
2024-05-23 13:19:02 +0900

    merge: auth webflux branch (#4)
    * init: 프로젝트 초기 설정
    
    * feat: 의존성 변경
    
    * feat: Entity 생성
    
    * feat: 종속성 변경
    
    * feat: Jpa Audit 옵션 추가
    
    * feat: ApiResponse 추가
    
    * feat: JWT Filter
    
    * feat: JWT Provider 추가
    
    액세스토큰, 리프레시 토큰 생성 검증 추가
    
    * feat: JWT Provider 추가
    
    액세스토큰, 리프레시 토큰 생성 검증 추가
    
    * feat: 예외 클래스 추가
    
    * feat: CORS 설정
    
    * feat: 로그인 기능 추가
    
    * feat: UserRepository 추가
    
    * feat: 소셜 ProviderFinder 추가
    
    * feat: 소셜 Provider 인터페이스 및 구현체 추가
    
    * feat: 로그인 API
    
    * feat: 카카오 Login OpenAPI 호출 로직 추가
    
    * feat: SpringSecurityConfig
    
    * feat: SocialProviderRepository 추가
    
    * feat: 토큰 DTO
    
    * feat: AuthService 추가
    
    액세스토큰 검증, 리프레시 토큰 발급
    
    * feat: 카카오 SocialDto 추가
    
    * feat: 인증 API 추가
    
    * refactor: 로그정리
    
    * feat: login uri 변경
    
    * feat: 최초로그인 수정
    
    * feat: 로그아웃 API
    
    * feat: added Dockerfile
    
    * feat: Entity 생성
    
    * feat: 종속성 변경
    
    * feat: Jpa Audit 옵션 추가
    
    * feat: ApiResponse 추가
    
    * feat: JWT Filter
    
    * feat: JWT Provider 추가
    
    액세스토큰, 리프레시 토큰 생성 검증 추가
    
    * feat: JWT Provider 추가
    
    액세스토큰, 리프레시 토큰 생성 검증 추가
    
    * feat: 예외 클래스 추가
    
    * feat: CORS 설정
    
    * feat: 로그인 기능 추가
    
    * feat: UserRepository 추가
    
    * feat: 소셜 ProviderFinder 추가
    
    * feat: 소셜 Provider 인터페이스 및 구현체 추가
    
    * feat: 로그인 API
    
    * feat: 카카오 Login OpenAPI 호출 로직 추가
    
    * feat: SpringSecurityConfig
    
    * feat: SocialProviderRepository 추가
    
    * feat: 토큰 DTO
    
    * feat: AuthService 추가
    
    액세스토큰 검증, 리프레시 토큰 발급
    
    * feat: 카카오 SocialDto 추가
    
    * feat: 인증 API 추가
    
    * refactor: 로그정리
    
    * feat: login uri 변경
    
    * feat: 최초로그인 수정
    
    * feat: 로그아웃 API
    
    * feat: test api 추가
    
    * feat: test security 제한 해제
    
    * feat: test 삭제
    
    * feat: oauth2 인증 이후 redirectURL 적용
    
    * fix: value path 수정
    
    * fix: value path 수정
    
    * fix: permit url 수정
    
    * fix: permit url 수정
    
    * fix: redis password 설정
    
    * feat: JWT 토큰에 social 정보 추가 저장
    
    * feat: 토큰 재발급 시 소셜정보 추가 저장
    
    * feat: Principal custom 객체 생성
    
    * feat: 카카오 로그아웃 로직 변경
    
    * feat: 로그아웃 로직 변경
    
    쿠키 초기화
    소셜 OAuth 로그아웃 로직 호출
    레디스 토큰 만료
    
    * feat: 로그인 시 닉네임 저장
    
    * feat: healthcheck 컨트롤러 생성
    
    * fix: 인증 객체 수정
    
    * fix: CORS 오리진 수정
    
    * hotfix: logout error fix
    
    * feat: Servlet -> Webflux
    
    * feat: Servlet -> Webflux
    
    * feat: precedent gateway
    
    * feat: login 이후 redirect 설정
    
    * feat: route test
    
    * feat: 액세스 토큰 재발급 로직 추가
    
    * feat: cors origin 설정 추가
    
    * feat: 라우팅 전체 완료
    
    * rename: 폴더 이름 변경
    
    * fix: 토큰 발급 수정
    
    * refactor: 주석 및 사용하지 않는 파일 정리
    
    ---------
    
    Co-authored-by: 박우철 <woochul102@gmail.com>
    Co-authored-by: 이경민 <kmeye0419@gmail.com>

---
+++ back-auth-v2/src/main/java/com/hellolaw/auth/filter/JWTAuthenticationFilter.java
@@ -0,0 +1,112 @@
+package com.hellolaw.auth.filter;
+
+import java.time.Duration;
+
+import org.springframework.http.HttpCookie;
+import org.springframework.http.ResponseCookie;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextImpl;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+
+import com.hellolaw.auth.service.AuthService;
+import com.hellolaw.auth.util.JWTProvider;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class JWTAuthenticationFilter implements WebFilter {
+	private final JWTProvider jwtProvider;
+	private final AuthService authService;
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+		log.info("JWT 필터 실행");
+		log.info(exchange.getRequest().getMethod().toString());
+		return getAccessToken(exchange.getRequest().getCookies())
+			.flatMap(accessToken -> {
+				if (jwtProvider.isValidateToken(accessToken)) {
+					return updateSecurityContext(accessToken)
+						.flatMap(securityContext -> chain.filter(exchange)
+							.contextWrite(
+								ReactiveSecurityContextHolder.withSecurityContext(Mono.just(securityContext))));
+				} else {
+					return validateRefreshTokenAndReIssueAccessToken(exchange, accessToken, chain);
+				}
+			})
+			.switchIfEmpty(chain.filter(exchange)) // 쿠키가 없는 경우 체인을 계속 진행
+			.onErrorResume(e -> {
+				log.error("Error during JWT filter processing", e);
+				return chain.filter(exchange);
+			});
+	}
+
+	private Mono<String> getAccessToken(MultiValueMap<String, HttpCookie> cookies) {
+		log.info("쿠키에서 액세스 토큰 추출 시도");
+		HttpCookie accessTokenCookie = cookies.getFirst("access-token");
+		if (accessTokenCookie == null) {
+			log.warn("액세스 토큰 쿠키가 존재하지 않습니다.");
+			return Mono.empty();
+		}
+		String accessToken = accessTokenCookie.getValue();
+		log.info("액세스 토큰 추출 성공: {}", accessToken);
+		return Mono.just(accessToken);
+	}
+
+	private Mono<SecurityContext> updateSecurityContext(String accessToken) {
+		return Mono.defer(() -> {
+			Authentication authentication = jwtProvider.getAuthentication(accessToken);
+			SecurityContextImpl securityContext = new SecurityContextImpl(authentication);
+			SecurityContextHolder.setContext(securityContext);
+			log.info("인증 정보로 SecurityContext 업데이트: {}", securityContext.getAuthentication().isAuthenticated());
+			return Mono.just(securityContext);
+		});
+	}
+
+	private Mono<String> reIssueAccessToken(String accessToken) {
+		log.info("액세스 토큰 재발급");
+		return Mono.just(jwtProvider.createAccessToken(jwtProvider.getId(accessToken),
+			jwtProvider.getProvider(accessToken)));
+	}
+
+	private void addAccessTokenCookie(ServerWebExchange exchange, String accessToken) {
+		log.info("액세스 토큰 쿠키에 더하기");
+		ResponseCookie cookie = ResponseCookie.from("access-token", accessToken)
+			.httpOnly(false)
+			.maxAge(Duration.ofDays(30))
+			.path("/")
+			.build();
+		exchange.getResponse().addCookie(cookie);
+	}
+
+	private Mono<Void> validateRefreshTokenAndReIssueAccessToken(ServerWebExchange exchange, String accessToken,
+		WebFilterChain chain) {
+		log.info("레디스 이용해서 재발급");
+		return authService.validateRefreshTokenInRedis(accessToken)
+			.flatMap(isValidRefreshToken -> {
+				if (isValidRefreshToken) {
+					return reIssueAccessToken(accessToken)
+						.flatMap(newAccessToken -> {
+							addAccessTokenCookie(exchange, newAccessToken);
+							return updateSecurityContext(newAccessToken)
+								.flatMap(securityContext -> chain.filter(exchange)
+									.contextWrite(
+										ReactiveSecurityContextHolder.withSecurityContext(Mono.just(securityContext))));
+						});
+				} else {
+					SecurityContextHolder.clearContext();
+					return chain.filter(exchange);
+				}
+			});
+	}
+}
